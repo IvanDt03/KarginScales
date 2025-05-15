a5c728cb62ac6e09f410c3ce1b7dfce9c8963ece
Ivan Dmitriev <vanya2003569@yandex.ru>
2025-05-15 21:16:48 +0300

    Добавить папку Views и перенести в нее файл главного окна, добавить класс ViewMOdelBase

---
+++ KarginScales/ViewModels/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KarginScales.ViewModels;
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
+    {
+        if (PropertyChanged is not null)
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual bool SetValue<T>(ref T field, T value, [CallerMemberName]string propertyName = "")
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+            return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
