1dce23014e95df379afc8d29b79edeb35c8461d8
Ivan Dmitriev <vanya2003569@yandex.ru>
2025-05-16 13:41:48 +0300

    Добавить файлы Polymer и DataPoint в Models

---
+++ KarginScales/Models/DataPoint.cs
@@ -0,0 +1,13 @@
+﻿namespace KarginScales.Models;
+
+public struct DataPoint
+{
+    public double Temperature { get; set; }
+    public double Gamma { get; set; }
+
+    public DataPoint(double temperature, double gamma)
+    {
+        Temperature = temperature; 
+        Gamma = gamma;
+    }
+}
---
+++ KarginScales/Models/Polymer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace KarginScales.Models;
+
+public class Polymer
+{
+    private string _name;
+    private double _minT;
+    private double _maxT;
+    private List<DataPoint> _data;
+
+    public Polymer(string name) : this(name, new List<DataPoint>()) { }
+
+    public Polymer(string name, List<DataPoint> data)
+    {
+        _name = name;
+        Initialize(data);
+    }
+
+    private void Initialize(List<DataPoint> data)
+    {
+        _data = data;
+        _minT = data.Min(p => p.Temperature);
+        _maxT = data.Max(p => p.Temperature);
+
+       
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public double MinT
+    {
+        get { return _minT; }
+    }
+
+    public double MaxT
+    {
+        get { return _maxT; }
+    }
+
+    public List<DataPoint> Data
+    {
+        get { return _data; }
+        set
+        {
+            Initialize(value);
+        }
+    }
+}
