ecd81fda177c0ba8355a47f3c751b9a7533ec616
Ivan Dmitriev <vanya2003569@yandex.ru>
2025-05-16 19:05:44 +0300

    Добавить реализацию ExcelDataService

--- KarginScales/Service/ExcelDataService.cs
+++ KarginScales/Service/ExcelDataService.cs
@@ -1,22 +1,54 @@
 ﻿using KarginScales.Models;
 using System.Collections.Generic;
 using ClosedXML.Excel;
+using System.IO;
+using System.Linq;
 
 namespace KarginScales.Service;
 
 public class ExcelDataService : IDataService
 {
+    private readonly XLWorkbook _book;
 
     public string FilePath { get; }
 
+    public ExcelDataService(string filePath)
+    {
+        if (!File.Exists(filePath))
+            throw new FileNotFoundException();
+
+        FilePath = filePath;
+        _book = new XLWorkbook(FilePath);
+    }
 
     public List<DataPoint> LoadDataPoint(string name)
     {
-        throw new System.NotImplementedException();
+        var result = new List<DataPoint>();
+
+        if (!_book.TryGetWorksheet(name, out var ws))
+            return result;
+
+        var table = ws.RangeUsed();
+
+        if (table == null)
+            return result;
+
+        for(int row = 2; row <= table.RowCount(); ++row)
+        {
+            result.Add(new DataPoint
+            {
+                Temperature = table.Cell(row, 1).GetDouble(),
+                Gamma = table.Cell(row, 2).GetDouble()
+            });
+        }
+
+        return result;
     }
 
     public List<Polymer> LoadNamesPolymer()
     {
-        throw new System.NotImplementedException();
+        return _book.Worksheets
+            .Select(s => new Polymer(s.Name))
+            .ToList();
     }
 }
